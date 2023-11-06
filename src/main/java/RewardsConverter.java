b90c4d5c52e7fd59b37d2b93f04ae00d2a0fb028
Vanshika Gupta <83124845+itzmishti@users.noreply.github.com>
2023-11-06 11:43:16 +0530

    Update RewardsConverter.java

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
 
-class RewardValue
-{
-    double v1,v2;
-    // RewardValue(double val)   {
-    //      v1=val;
-    // }
-    RewardValue(double cashValue)
-    {
-         v2= cashValue;
+ class RewardValue {
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
-    double getCashValue(double val)
-    {
-        return v2;
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
-    double getMilesValue()
-    {
-        return v2*0.0035;
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 };
 public class RewardsConverter {
